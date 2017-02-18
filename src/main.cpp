3a23b3c5d9090a89110a46fca10f4f2eb4918521
Josh Harwood <bocharwood@googlemail.com>
2017-02-18 16:01:40 +0000

    initial, gps library, debug

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <TinyGPS.h>
+
+/*
+Serial -> DEBUG
+Serial1 -> GPS
+Serial2 -> LoRaWAN
+*/
+
+#define DEBUG 1
+const int debug_baud = 9600;
+const int gps_baud = 9600;
+
+TinyGPS gps;
+
+void setup(){
+  if(DEBUG) Serial.begin(debug_baud);
+  /* Initialise uart communication to GPS module */
+  if(DEBUG) Serial.print("Initialising GPS UART...");
+  Serial1.begin(gps_baud);
+  if(DEBUG) Serial.print("Done\n");
+
+}
+void loop(){
+  if(DEBUG) Serial.println("Hello World!");
+}

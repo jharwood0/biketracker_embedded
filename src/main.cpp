ae9624326d53fe8f0f563c19a64f36efa0d57a48
Josh Harwood <bocharwood@googlemail.com>
2017-02-18 18:20:14 +0000

    RN2483 lib import

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <TinyGPS.h>
+#include "../lib/RN2483/src/rn2xx3.h"
 
 /*
 Arduino port
@@ -13,6 +14,7 @@ const int debug_baud = 9600;
 const int gps_baud = 9600;
 
 TinyGPS gps;
+rn2xx3 LoRaWAN(Serial1);
 
 void setup(){
   if(DEBUG) Serial.begin(debug_baud);
@@ -21,7 +23,13 @@ void setup(){
   Serial1.begin(gps_baud);
   if(DEBUG) Serial.print("Done\n");
 
+  /* Initialise uart communication to LoRaWAN module */
+  if(DEBUG) Serial.print("Initialising LoRaWAN UART...");
+  Serial1.begin(9600); //serial port to radio
+  Serial1.flush();
+  LoRaWAN.autobaud();
+  if(DEBUG) Serial.print("Done\n");
 }
 void loop(){
-  if(DEBUG) Serial.println("Hello World!");
+  if(DEBUG) Serial.println(LoRaWAN.deveui());
 }

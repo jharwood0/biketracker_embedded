01d56737a07a647ed5d36d9a56a4eb64edfbfd85
Josh Harwood <bocharwood@googlemail.com>
2017-02-19 15:07:09 +0000

    debug formatting

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <libmaple/scb.h>
 
 /*
-Arduino port
+STM32 port mappings
 UART1 -> Serial  -> DEBUG
 UART2 -> Serial1 -> GPS
 UART3 -> Serial2 -> LoRaWAN
@@ -17,10 +17,12 @@ String APPKEY = "23D8583ACF5A8B628540A53F0A18876D";
 
 #define DEBUG 1
 #define ENABLE_GPS 1
-#define ENABLE_LORAWAN 0
+#define ENABLE_LORAWAN 1
 
 const int debug_baud = 9600;
 const int gps_baud = 9600;
+
+bool LoRaWAN_connection = false;
 
 float flat, flon;
 unsigned long age;
@@ -54,44 +56,52 @@ void STM32_sleep(uint8_t i){
 }
 
 void setup(){
+  /* give time to attach debug monitor */
+  delay(5000);
+
   if(DEBUG) Serial.begin(debug_baud);
   /* Initialise uart communication to GPS module */
   if(ENABLE_GPS){
-    if(DEBUG) Serial.print("Initialising GPS UART...");
+    if(DEBUG) Serial.print("[GPS] Initialising UART...");
     Serial1.begin(gps_baud);
     if(DEBUG) Serial.print("Done\n");
   }
   /* Initialise uart communication to LoRaWAN module */
   if(ENABLE_LORAWAN){
-    if(DEBUG) Serial.print("Initialising LoRaWAN UART...");
+    if(DEBUG) Serial.print("[LoRaWAN] Initialising UART...");
     Serial2.begin(9600); //serial port to radio
     Serial2.flush();
     LoRaWAN.autobaud();
     if(DEBUG) Serial.print("Done\n");
     /* Join LoRaWAN */
-    bool join_result = LoRaWAN.initOTAA(APPEUI, APPKEY);
-    while(!join_result){
-      if(DEBUG) Serial.println("Unable to join LoRaWAN network");
-      delay(10000); //delay a 10 seconds before retry
-      join_result = LoRaWAN.init();
+    if(DEBUG) Serial.print("[LoRaWAN] Connecting...");
+    LoRaWAN_connection = LoRaWAN.initOTAA(APPEUI, APPKEY);
+    if(DEBUG){
+      if(LoRaWAN_connection){
+        Serial.print("Success!\n");
+      }else{
+        Serial.print("Failed\n");
+      }
     }
-    if(DEBUG) Serial.println("Connected to LoRaWAN");
   }
 }
 
 void loop(){
   if(ENABLE_GPS){
     if(DEBUG) Serial.print("[GPS] Reading UART...\n");
-    while (Serial1.available())
-    {
+    if(DEBUG) delay(1000); /* Wait for Serial to clear for debug */
+    while (!Serial1.available()){}
+    String out = "";
+    while (Serial1.available()){
       uint8_t c = Serial1.read();
-      if(DEBUG) Serial.print(char(c));
+      out += char(c);
       if (gps.encode(c))
       {
         new_gps_data = true;
       }
     }
-
+    if(DEBUG) Serial.println(out);
+    if(DEBUG) delay(1000); /* Wait for Serial to clear for debug */
     if(new_gps_data){
       if(DEBUG) Serial.print("[GPS] New data\n");
       gps.f_get_position(&flat, &flon, &age);
@@ -106,10 +116,22 @@ void loop(){
     }
   }
   if(ENABLE_LORAWAN){
-    LoRaWAN.tx("!");
+    if(!LoRaWAN_connection){
+      if(DEBUG) Serial.print("[LoRaWAN] Connecting...");
+      LoRaWAN_connection = LoRaWAN.init();
+      if(DEBUG){
+        if(LoRaWAN_connection){
+          Serial.print("Success!\n");
+        }else{
+          Serial.print("Failed\n");
+        }
+      }
+    }else{
+      LoRaWAN.tx("!");
+    }
     if(DEBUG) Serial.println(LoRaWAN.deveui());
   }
-  delay(1000);
+  delay(5000);
   /* STM32 sleep */
   //STM32_sleep(1); /* engange STM32 stanby for 5 seconds */
 }
